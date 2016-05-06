24e2679f9d2ce0f8c8bdc74d91b2dbcaee71f064
hywon53 <hywon53@student.monash.edu>
2016-05-06 11:26:59 +0800

    a+b + a-b
    Addition and subtraction function

--- Subtraction.js
+++ Subtraction.js
@@ -3,3 +3,12 @@ function subtraction (a,b){
     console.log(result)
     
 }
+
+function addition (a,b)
+{
+    var result = a + b
+    console.log(result)
+}
+
+final_result = addition (1,2) + substraction(2,1);
+console.log(final_result)
